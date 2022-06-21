16cc810169d433b84af2b43a2781ad67f10ea9d8
Mohit patel <mohitpatel1932@gmail.com>
2022-06-21 11:05:08 +0530

    commited

---
+++ index.js
@@ -0,0 +1,14 @@
+var express =require('express')
+var bodyParser=require('body-parser');
+var mongoose=require('mongoose');
+var router=require('./route');
+
+var app=express();
+app.use('/api',router);
+app.listen(process.env.port,()=>{
+    console.log("server started");
+})
+
+
+//process.env.port
+

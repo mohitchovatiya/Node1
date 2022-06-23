20a390dbea3115efd736f90f654fe8f118fcd7af
Mohit patel <mohitpatel1932@gmail.com>
2022-06-24 00:32:54 +0530

    commit

--- route.js
+++ route.js
@@ -1,7 +1,7 @@
 var express=require('express');
 const { default: mongoose } = require('mongoose');
 var router=express.Router();
-mongoose.connect("mongodb")
+mongoose.connect("mongodb://mohit:mohit@cluster0.vsafa9r.mongodb.net/?retryWrites=true&w=majority")
 
 router.get("/",(req,res)=>{
     res.sendFile("index.html",

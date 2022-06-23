7233a506f69ad3ef9f806d906173374f98fbd59c
Mohit patel <mohitpatel1932@gmail.com>
2022-06-23 22:31:12 +0530

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var router=require('./route');
 
 var app=express();
 app.use('/api',router);
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log("server started");
 })
 
--- route.js
+++ route.js
@@ -1,5 +1,7 @@
 var express=require('express');
+const { default: mongoose } = require('mongoose');
 var router=express.Router();
+mongoose.connect("mongodb")
 
 router.get("/",(req,res)=>{
     res.sendFile("index.html",
